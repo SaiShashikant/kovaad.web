76241298ca6f185a7af3897cb4cd09ff14d78ca0
Sai Shashikant <104009626+SaiShashikant@users.noreply.github.com>
2024-11-28 14:38:36 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Set the basePath for deployment in a subdirectory (GitHub Pages URL)
-  basePath: '/saishashikant',  // Replace 'repository-name' with your actual GitHub repository name
-
-  // Use assetPrefix to handle static assets (like images) correctly in a subdirectory
-  assetPrefix: '/saishashikant/',  // Replace with your GitHub repository name
-
-  // Optional: Enable trailing slash if needed for your URLs
-  // exportTrailingSlash: true,
+  /* config options here */
 };
 
 export default nextConfig;
