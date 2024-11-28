6ed8c959cd267c857141cff16fd292eb69ce0e38
Sai Shashikant <104009626+SaiShashikant@users.noreply.github.com>
2024-11-28 14:08:24 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,41 @@
-import type { NextConfig } from "next";
+const nextConfig = {
+    webpack: (config: { module: { rules: { test: RegExp; use: string | { loader: string; options: { minimize: boolean; }; }[]; }[]; }; }) => {
+        config.module.rules.push({
+            test: /\.node$/,
+            use: 'node-loader',
+        });
 
-const nextConfig: NextConfig = {
-  // Set the basePath for deployment in a subdirectory (GitHub Pages URL)
-  basePath: '/saishashikant.github.io',  // Replace 'repository-name' with your actual GitHub repository name
+        // Add rule for .html files
+        config.module.rules.push({
+            test: /\.html$/,
+            use: [
+                {
+                    loader: 'html-loader',
+                    options: {minimize: true},
+                },
+            ],
+        });
 
-  // Use assetPrefix to handle static assets (like images) correctly in a subdirectory
-  assetPrefix: '/saishashikant.github.ios/',  // Replace with your GitHub repository name
+        return config;
+    },
 
-  // Optional: Enable trailing slash if needed for your URLs
-  // exportTrailingSlash: true,
+    async headers() {
+        return [
+            {
+                source: '/api/:path*',
+                headers: [
+                    {key: 'Access-Control-Allow-Origin', value: '*'}, // Replace '*' with your allowed origin
+                    {key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT'},
+                    {key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'},
+                ],
+            },
+        ];
+    },
+
+    images: {
+        domains: ['firebasestorage.googleapis.com', 'beta.kovaad.ai', 'chat.kovaad.ai', 'alpha.kovaad.ai'], // Allow images from Firebase Storage
+    },
+    swcMinify: true, // Enable SWC minification
 };
 
-export default nextConfig;
+module.exports = nextConfig;
